edcdef07dc213c56bd6daa01ec5d566fd0bf6930
base1234 <30730544+base1234@users.noreply.github.com>
2017-08-16 22:46:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName',function(req,res){
     //articleName==article-one
     //articles[articleName]=[]content object for article one
-  res.send(createTemplate(articles[articleName]));
+  res.send(createTemplate(articles['article-one']));
   var articleName = req.params.articleName;
 });
 
